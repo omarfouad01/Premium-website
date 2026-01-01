58cd2dfb39c813a3eca288a788d0a18821cce33f
Abdallah_Gaber <aboodymoha123@gmail.com>
2026-01-01 18:18:09 +0200

    adding new account

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ibliyrfsakhktwbhclas.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlibGl5cmZzYWtoa3R3YmhjbGFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2NzExNjUsImV4cCI6MjA4MjI0NzE2NX0.iuGZSBu-GN_0mKu5Ea7MNZVdlUkKxAA1oGWQp0Mc5S0'
+const supabaseUrl = 'https://dfmgbymdehmgissohtrh.supabase.co'
+const supabaseAnonKey = 'sb_publishable_-BvBRjJTBXPa5ib249071Q_rUFGRRMf'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
